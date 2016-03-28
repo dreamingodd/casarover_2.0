7e14d487cf343683ff91df97b1680acb2e5a236b
Ye, Wenda <memorywilllast@sina.com>
2016-03-28 15:04:31 +0800

    DONE: casa edit.

--- app/Attachment.php
+++ app/Attachment.php
@@ -10,8 +10,5 @@ class Attachment extends Model
 	const UPDATED_AT = 'update_time';
 	const CREATED_AT = 'update_time';
     protected $table = "attachment";
-    const DELETED_AT = 'updated_at';
-    const UPDATED_AT = 'update_time';
-    const CREATED_AT = 'update_time';
     protected $fillable = array('filepath');
 }
