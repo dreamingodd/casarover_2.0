d8033fc4921211618f1d92862c0907776d7ce608
Ye_Wenda <ye_wenda@Ye-WendadeMacBook-Air.local>
2016-04-14 21:31:06 +0800

    换行符修改转换失败，直接提交成功的tricky issue.

--- resources/assets/js/themeEdit.js
+++ resources/assets/js/themeEdit.js
@@ -5,12 +5,12 @@ function sed(){
 $(document).ready(function(){
     // content 显示设置 替换"<br/>"为"\n", 当然上传的时候也做了相反的替换
     $('textarea').each(function() {
-        $(this).html(BRtoLF($(this).html()));
+        $(this).html(BRtoLF($(this).val()));
     });
     $('#themeSubmitBtn').click(function(){
         $('textarea').each(function() {
-            $(this).html(LFtoBR($(this).html()));
+            $('textarea')[0].value = LFtoBR($(this).val());
         });
         $('#themeForm').submit();
-    })
-})
+    });
+});
