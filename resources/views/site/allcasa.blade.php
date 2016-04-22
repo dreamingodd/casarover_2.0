08398f4767bb826820f64e76d68406008b303cc2
gs <511904266@qq.com>
2016-04-22 13:41:51 +0800

    autoallcasa

--- resources/views/site/allcasa.blade.php
+++ resources/views/site/allcasa.blade.php
@@ -22,129 +22,120 @@
             </li>
         </ul>
     </div>
-    <div class="main">
-        <div class="screen">
-            <div class="case">
-                <p>城市<a href="javascript:void(0)" class='show'>显示全部</a></p>
-                <ul class="casa">
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
-                    <li><a>杭州</a></li>
+    <div class="all">
+        <div class="main">
+            <div class="screen">
+                <div class="case">
+                    <p>城市<a href="javascript:void(0)" class='show'>显示全部</a></p>
+                    <ul class="casa">
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                        <li><a>杭州</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="screen">
+                <div class="case">
+                    <p>区域<a href="javascript:void(0)" class='show'>显示全部</a></p>
+                    <ul class="casas">
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                        <li><a>临安</a></li>
+                    </ul>
+                </div>
+            </div>
+                <a href="javascript:void(0)" class="right-float-top" id="toTop" >返回顶部</a>
+                <a href="javascript:void(0)" class="right-float-middle" id="advice">意见反馈</a>
+                <a href="javascript:void(0)" class="right-float-bottom" id="qrcode"></a>
+        </div>
+        <section>
+            <div class="loader">
+                <div class="loader-inner line-scale">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+            <div class="card">
+                <img src="/assets/images/test/1.png" alt="">
+                <h3>法云安曼精品宿舍</h3>
+                <p>地址：西湖区灵隐支路白乐桥246号</p>
+                <p>标签：</p>
+                <ul>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                    <li>小清新</li>
                 </ul>
             </div>
-        </div>
-        <div class="screen">
-            <div class="case">
-                <p>区域<a href="javascript:void(0)" class='show'>显示全部</a></p>
-                <ul class="casas">
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
-                    <li><a>临安</a></li>
+            <div class="card">
+                <img src="/assets/images/test/1.png" alt="">
+                <h3>法云安曼精品宿舍</h3>
+                <p>地址：西湖区灵隐支路白乐桥246号</p>
+                <p>标签：</p>
+                <ul>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                    <li>小清新</li>
                 </ul>
             </div>
-        </div>
-            <a href="javascript:void(0)" class="right-float-top" id="toTop" >返回顶部</a>
-            <a href="javascript:void(0)" class="right-float-middle" id="advice">意见反馈</a>
-            <a href="javascript:void(0)" class="right-float-bottom" id="qrcode"></a>
+            <div class="card">
+                <img src="/assets/images/test/1.png" alt="">
+                <h3>法云安曼精品宿舍</h3>
+                <p>地址：西湖区灵隐支路白乐桥246号</p>
+                <p>标签：</p>
+                <ul>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                </ul>
+            </div>
+            <div class="card">
+                <img src="/assets/images/test/1.png" alt="">
+                <h3>法云安曼精品宿舍</h3>
+                <p>地址：西湖区灵隐支路白乐桥246号</p>
+                <p>标签：</p>
+                <ul>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                    <li>小清新</li>
+                </ul>
+            </div>
+        </section>
     </div>
-    <section>
-        <div class="loader">
-            <div class="loader-inner line-scale">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </div>
-        <div class="card">
-            <img src="/assets/images/test/1.png" alt="">
-            <h3>法云安曼精品宿舍</h3>
-            <p>地址：西湖区灵隐支路白乐桥246号</p>
-            <p>标签：</p>
-            <ul>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-            </ul>
-            {{--<span>预订</span>--}}
-        </div>
-        <div class="card">
-            <img src="/assets/images/test/1.png" alt="">
-            <h3>法云安曼精品宿舍</h3>
-            <p>地址：西湖区灵隐支路白乐桥246号</p>
-            <p>标签：</p>
-            <ul>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-            </ul>
-            {{--<span>预订</span>--}}
-        </div>
-        <div class="card">
-            <img src="/assets/images/test/1.png" alt="">
-            <h3>法云安曼精品宿舍</h3>
-            <p>地址：西湖区灵隐支路白乐桥246号</p>
-            <p>标签：</p>
-            <ul>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-            </ul>
-            {{--<span>预订</span>--}}
-        </div>
-        <div class="card">
-            <img src="/assets/images/test/1.png" alt="">
-            <h3>法云安曼精品宿舍</h3>
-            <p>地址：西湖区灵隐支路白乐桥246号</p>
-            <p>标签：</p>
-            <ul>
-                <li>小清新</li>
-                <li>小清新</li>
-                <li>小清新</li>
-            </ul>
-            {{--<span>预订</span>--}}
-        </div>
-    </section>
 @endsection
