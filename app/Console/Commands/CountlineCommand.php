a7391ac1666b026a270858f26a4f3d9c7e0cbfff
Ye_WD <memorywilllast@gmail.com>
2016-04-26 14:33:36 +0800

    use namespace require test

---
+++ app/Console/Commands/CountlineCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Console\Commands\Processors\Countline;
+
+class CountlineCommand extends Command
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ywd:countline';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    use Countline;
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->countline();
+    }
+}
