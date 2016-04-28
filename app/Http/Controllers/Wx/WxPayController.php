decb7288eda85843293b302ff1b803697b54b04a
Ye_WD <memorywilllast@gmail.com>
2016-04-28 10:43:33 +0800

    wxapy 9

--- app/Http/Controllers/Wx/WxPayController.php
+++ app/Http/Controllers/Wx/WxPayController.php
@@ -17,36 +17,7 @@ class WxPayController extends Controller
      */
     public function wxOrder($orderId) {
         $order = WxOrder::find($orderId);
-        ini_set('date.timezone','Asia/Shanghai');
-        //error_reporting(E_ERROR);
-        require_once app_path()."/lib/WxpayAPI/lib/WxPay.Api.php";
-        require_once app_path()."/lib/WxpayAPI/example/WxPay.JsApiPay.php";
-
-        // 微信API日志 //初始化日志
-        // require_once app_path().'/lib/WxpayAPI/example/log.php';
-        // $logHandler= new CLogFileHandler(app_path()."/lib/WxpayAPI/logs/".date('Y-m-d').'.log');
-        // $log = Log::Init($logHandler, 15);
-
-        //①、获取用户openid
-        $tools = new JsApiPay();
-        $openId = $tools->GetOpenid();
-
-        //②、统一下单
-        $input = new WxPayUnifiedOrder();
-        $input->SetBody("test");
-        $input->SetAttach("test");
-        $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
-        $input->SetTotal_fee($order->total);
-        $input->SetTime_start(date("YmdHis"));
-        $input->SetTime_expire(date("YmdHis", time() + 600));
-        $input->SetGoods_tag("test");
-        $input->SetNotify_url("http://www.casarover.com/WxpayAPI/example/notify.php");
-        $input->SetTrade_type("JSAPI");
-        $input->SetOpenid($openId);
-        $order = WxPayApi::unifiedOrder($input);
-        $jsApiParameters = $tools->GetJsApiParameters($order);
-        Log::info("WxOrder:" . $jsApiParameters);
-        return view("wx.wxTemp", compact('order', 'jsApiParameters'));
+        return view("wx.wxTemp", compact('order'));
     }
 
     /**
