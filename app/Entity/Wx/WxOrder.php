32ca36e161b5249589042585d2767943cd67e929
dreamingodd <memorywilllast@gmail.com>
2016-05-05 13:34:29 +0800

    Order status update

--- app/Entity/Wx/WxOrder.php
+++ app/Entity/Wx/WxOrder.php
@@ -12,8 +12,7 @@ class WxOrder extends Model
     const PAY_STATUS_REFUNDED = 3;
     const RESERVE_STATUS_NO = 0;
     const RESERVE_STATUS_YES = 1;
-    const RESERVE_STATUS_PROCESS = 2;
-    const RESERVE_STATUS_FAIL = 3;
+    const RESERVE_STATUS_FAIL = 2;
     const CONSUME_STATUS_NO = 0;
     const CONSUME_STATUS_YES = 1;
     const CONSUME_STATUS_EXPIRED = 2;
