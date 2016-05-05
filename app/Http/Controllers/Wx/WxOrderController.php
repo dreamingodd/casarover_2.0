16da57079a4d77d024595a37a39822555cb20dc5
dreamingodd <memorywilllast@gmail.com>
2016-05-05 13:29:52 +0800

    Wx Order Info Update
    1. Wxpay transaction id
    2. Add price to order item, total to order

--- app/Http/Controllers/Wx/WxOrderController.php
+++ app/Http/Controllers/Wx/WxOrderController.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Controller;
 use App\Entity\Wx\WxOrder;
 use App\Entity\Wx\WxUser;
 use App\Entity\Wx\WxOrderItem;
+use App\Entity\Wx\WxRoom;
 
 class WxOrderController extends Controller
 {
@@ -24,18 +25,24 @@ class WxOrderController extends Controller
             $wxOrder = new WxOrder();
             $wxOrder->wx_user_id = WxUser::find(Session::get('wx_user_id'))->id;
             $wxOrder->save();
-            $wxOrder->order_id = Config::get("casarover.wx_shopid") . '-' . $wxOrder->id;
-            $wxOrder->save();
             if (empty($reservedRooms)) {
                 return "没有选购商品！";
             }
+            $total = 0;
             foreach ($reservedRooms as $reservedRoom) {
                 $wxOrderItem = new WxOrderItem();
                 $wxOrderItem->wx_order_id = $wxOrder->id;
                 $wxOrderItem->wx_room_id = $reservedRoom['id'];
+                $wxOrderItem->price = WxRoom::find($reservedRoom['id'])->price;
                 $wxOrderItem->quantity = $reservedRoom['quantity'];
                 $wxOrderItem->save();
+                $total += $wxOrderItem->price * $wxOrderItem->quantity;
             }
+
+            // update order info
+            $wxOrder->order_id = Config::get("casarover.wx_shopid") . '-' . $wxOrder->id;
+            $wxOrder->total = $total;
+            $wxOrder->save();
             DB::commit();
             return response()->json(['orderId' => $wxOrder->id]);
         } catch (Exception $ex) {
