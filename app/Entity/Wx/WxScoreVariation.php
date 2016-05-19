b90d1605b342fe9b10c847131481cfe628792ddd
dreamingodd <memorywilllast@gmail.com>
2016-05-19 14:47:59 +0800

    wx membership consume score whilst user pay the order.
    下单时消费积分

--- app/Entity/Wx/WxScoreVariation.php
+++ app/Entity/Wx/WxScoreVariation.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class WxScoreVariation extends Model
 {
+    const TYPE_ORDER = 1;
+    const TYPE_ACTIVITY = 2;
+    const SCORE_PERCENT = 0.1;
     protected $table = "wx_score_variation";
 
 }
