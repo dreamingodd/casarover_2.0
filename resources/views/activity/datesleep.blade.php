56abbc58091725527f26445a703a488128aabb67
gs <511904266@qq.com>
2016-05-26 14:36:49 +0800

    activity datesleep modify

--- resources/views/activity/datesleep.blade.php
+++ resources/views/activity/datesleep.blade.php
@@ -6,12 +6,12 @@
 @section('body')
 <div class="bg">
     <div class="banner">
-        <img src="\assets\images\activity\datebanner.png" alt="">
+        <img src="/assets/images/activity/datebanner.png" alt="">
     </div>
         <div class="main">
             <div class="maincon clear">
                 <div class="left">
-                    <img src="\assets\images\activity\user.jpg" alt="">
+                    <img src="/assets/images/activity/user.jpg" alt="">
                 </div>
                 <div class="right">
                     <div class="righttop">
@@ -25,29 +25,45 @@
             </div>
         </div>
         <div class="button">
-            <a class="helpsleep">帮他约睡</a>
-            <a href="\activity">我也约</a>
+            {{--<a class="helpsleep">帮他约睡</a>--}}
+            {{--<a href="/activity">我也约</a>--}}
+            <a class="invite">邀请好友帮我约</a>
+            <a  class="date">我要约</a>
             <a href="rank">排行</a>
         </div>
     </div>
     <div class="detail">
         <div class="detailcon clear">
-            <img src="\assets\images\activity\user.jpg" alt="">
+            <img src="/assets/images/activity/user.jpg" alt="">
             <p>已成功帮助XXX向XXX
                 发起约睡。</p>
-            <a href="\activity">我也约</a>
+            <a href="/activity">我也约</a>
         </div>
-        <img src="\assets\images\activity\close.png" alt="" class="md-close">
+        <img src="/assets/images/activity/close.png" alt="" class="md-close">
     </div>
+<div class="invitecon">
+    <div class="share">
+        <img src="/assets/images/activity/wxshare.png" alt="">
+    </div>
+    <div class="arrow">
+        <img src="/assets/images/activity/arrow.png" alt="">
+    </div>
+    <img src="/assets/images/activity/close.png" alt="" class="md-close">
+</div>
     <script>
         $(function () {
             $('.helpsleep').click(function () {
                 $('.bg').addClass('blur');
                 $('.detail').show();
             });
+            $('.invite').click(function () {
+                $('.bg').addClass('blur');
+                $('.invitecon').show();
+            });
             $('.md-close').click(function () {
                 $('.bg').removeClass('blur');
                 $('.detail').hide();
+                $('.invitecon').hide();
             });
         });
 
