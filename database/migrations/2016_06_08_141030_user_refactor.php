f9e407c779fe5af9281a027aaf9c5e8bbcc22ab8
dreamingodd <memorywilllast@gmail.com>
2016-06-08 18:34:39 +0800

    Add Dtabase Diagram
    Refactor user

---
+++ database/migrations/2016_06_08_141030_user_refactor.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Trying to integrate user tables.
+ */
+class UserRefactor extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('user');
+        Schema::rename('wx_user', 'user');
+        Schema::table('user', function (Blueprint $t) {
+            // Casarover:0, Wechat:1, QQ:2
+            $t->tinyInteger('type')->after('id');
+        });
+        Schema::table('wx_order', function(Blueprint $t) {
+            $t->renameColumn('wx_user_id', 'user_id');
+        });
+        Schema::table('wx_vote', function(Blueprint $t) {
+            $t->renameColumn('wx_user_id', 'user_id');
+        });
+        Schema::table('wx_membership', function(Blueprint $t) {
+            $t->renameColumn('wx_user_id', 'user_id');
+        });
+        Schema::table('wx_user_casa_18', function(Blueprint $t) {
+            $t->renameColumn('wx_user_id', 'user_id');
+        });
+        Schema::table('wx_bind', function(Blueprint $t) {
+            $t->renameColumn('wx_user_id', 'user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('wx_order', function(Blueprint $t) {
+            $t->renameColumn('user_id', 'wx_user_id');
+        });
+        Schema::table('wx_vote', function(Blueprint $t) {
+            $t->renameColumn('user_id', 'wx_user_id');
+        });
+        Schema::table('wx_membership', function(Blueprint $t) {
+            $t->renameColumn('user_id', 'wx_user_id');
+        });
+        Schema::table('wx_user_casa_18', function(Blueprint $t) {
+            $t->renameColumn('user_id', 'wx_user_id');
+        });
+        Schema::table('wx_bind', function(Blueprint $t) {
+            $t->renameColumn('user_id', 'wx_user_id');
+        });
+        Schema::table('user', function (Blueprint $t) {
+            // Casarover:0, Wechat:1, QQ:2
+            $t->dropColumn('type');
+        });
+        Schema::rename('user', 'wx_user');
+        Schema::create('user', function(Blueprint $t) {
+            $t->bigIncrements('id');
+        });
+    }
+}
