d2fb287d23fd82794fcacd4811b8823dc515c7b6
gs <511904266@qq.com>
2016-06-14 17:40:22 +0800

    vacation front 90%

--- resources/views/wx/cardBook.blade.php
+++ resources/views/wx/cardBook.blade.php
@@ -1,7 +1,7 @@
 @extends('wxBase')
-@section('title','申请记录')
+@section('title','民宿预订')
 @section('head')
-    <link href="/assets/css/cardApply.css" rel="stylesheet"/>
+    <link href="/assets/css/cardBook.css" rel="stylesheet"/>
 @stop
 @section('nav')
     <a href="/wx/user" id="navleft" class="glyphicon glyphicon-chevron-left"></a>
@@ -10,29 +10,24 @@
 @stop
 @section('body')
     <div class="main">
-        <h2>申请记录</h2>
+        <h2>民宿预订</h2>
         <div class="case clear" >
             <input type="hidden" value="" class="casaId">
-            <div class="info">
-                <p>申请人姓名：小红</p>
-                <p>电话号码：12345678901</p>
-            </div>
             <div class="casecon clear">
                 <img src="http://casarover.oss-cn-hangzhou.aliyuncs.com/casa/casa_20160518-163558-932r9132.jpg"" alt="">
                 <div class="article">
                     <h3>梅皋巫山居</h3>
+                    <img src="/assets/images/sign.png" alt="" >
                     <div class="articlecon">
-                        <p>备注:我是你表姐</p>
-                        <span>申请间数: <i>3</i></span>
-                        <div class="click">
-                            回复
-                        </div>
+                        <p>湖州-德州-莫干山镇</p>
+                        <p>截至日期:2017年6月1日</p>
+                        <span>预订间数：<i id="reduce">-</i> <i id="number">3</i> <i id="add">+</i></span>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
+    <a class="sub" href="tel:{{Config::get('config.help_telephone')}}">提&nbsp;&nbsp;交</a>
     <script>
     </script>
 @stop
