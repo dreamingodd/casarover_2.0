ce8a4007e05fa99214e6d2d6cc4ebb31ba30f70a
dreamingodd <memorywilllast@gmail.com>
2016-06-14 15:19:58 +0800

    Order list & room edit refactoring

--- app/Http/Controllers/Wx/WxRoomController.php
+++ app/Http/Controllers/Wx/WxRoomController.php
@@ -2,53 +2,85 @@
 
 namespace App\Http\Controllers\Wx;
 
+use DB;
+use Exception;
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Entity\Product;
 use App\Entity\Wx\WxCasa;
-use App\Entity\Wx\WxRoom;
-use App\Entity\Wx\WxRoomDate;
 
+/**  */
 class WxRoomController extends Controller
 {
+    /**
+     * Get the rooms(products) of a wx casa.
+     * @param int $id
+     */
     public function show($id) {
-        $wxRooms = WxCasa::find($id)->wxRooms;
         $wxCasaId = $id;
-        return view('backstage.wxRoom', compact('wxRooms', 'wxCasaId'));
+        $rooms = WxCasa::find($wxCasaId)->rooms;
+        return view('backstage.wxRoom', compact('rooms', 'wxCasaId'));
     }
+
+    /**
+     * Edit the rooms(products) of a wx casa.
+     * @param Request $request
+     */
     public function edit(Request $request) {
-        $wxCasaId = $request->input('wxCasaId');
-        $rawRooms = json_decode($request->input('wxRooms'));
-        $wxCasa = WxCasa::find($wxCasaId);
-        $wxRooms = $this->createRooms($rawRooms);
-        $wxCasa->wxRooms()->saveMany($wxRooms);
+        DB::beginTransaction();
+        try {
+            $wxCasaId = $request->input('wxCasaId');
+            $rawRooms = json_decode($request->input('wxRooms'));
+            $wxCasa = WxCasa::find($wxCasaId);
+            $rooms = $this->createRooms($rawRooms);
+            $wxCasa->rooms()->saveMany($rooms);
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            dd($e);
+        }
         return redirect('/back/wx');
     }
-    public function date($id) {
-        $date=WxRoomDate::where('room_id',$id)->get();
-        $wxRoom = WxRoom::find($id);
-        return view('backstage.wxRoomDate',compact('date','wxRoom'));
-    }
-    public function postdate(Request $request , $id) {
-        $save= $request->all();
-        $count=count($save['year']);
-        $wxRooms = WxRoom::find($id);
-        $casaId=$wxRooms->wxCasa->id;
-        for($i=0;$i<$count;$i++){
-            $date=WxRoomDate::where('room_id',$id)->where('year',$save['year'][$i])->where
-            ('month',$save['month'][$i])->first();
-            if(!$date){
-            $date= new WxRoomDate;
-            }
-            $date->room_id=$save['room_id'];
-            $date->year=$save['year'][$i];
-            $date->month=$save['month'][$i];
-            $date->day=$save['day'][$i];
-            $date->save();
-        }
-        return redirect("/back/wx/room/$casaId");
-    }
+
+    /** Temporarily unused. */
+    // /**
+    //  * @param int $id
+    //  */
+    // public function date($id) {
+    //     $date = WxRoomDate::where('room_id',$id)->get();
+    //     $wxRoom = Product::find($id);
+    //     return view('backstage.wxRoomDate',compact('date','wxRoom'));
+    // }
+
+    // /**
+    //  * @param Request $request
+    //  * @param int $id
+    //  */
+    // public function postdate(Request $request , $id) {
+    //     $save= $request->all();
+    //     $count=count($save['year']);
+    //     $rooms = Product::find($id);
+    //     $casaId=$rooms->wxCasa->id;
+    //     for($i=0;$i<$count;$i++){
+    //         $date=WxRoomDate::where('room_id',$id)->where('year',$save['year'][$i])->where
+    //         ('month',$save['month'][$i])->first();
+    //         if(!$date){
+    //         $date= new WxRoomDate;
+    //         }
+    //         $date->room_id=$save['room_id'];
+    //         $date->year=$save['year'][$i];
+    //         $date->month=$save['month'][$i];
+    //         $date->day=$save['day'][$i];
+    //         $date->save();
+    //     }
+    //     return redirect("/back/wx/room/$casaId");
+    // }
+
+    /**
+     * Create rooms(products) according to the json data array.
+     * @param array $rawRooms
+     */
     private function createRooms($rawRooms) {
         $rooms = array();
         foreach ($rawRooms as $rawRoom) {
@@ -58,12 +90,16 @@ class WxRoomController extends Controller
         return $rooms;
     }
 
+    /**
+     * Create room(product type is TYPE_CASA_ROOM) according to the json data.
+     * @param object $rawRoom
+     */
     private function createRoom($rawRoom) {
         $room = null;
         if (isset($rawRoom->id)) {
-            $room = WxRoom::find($rawRoom->id);
+            $room = Product::find($rawRoom->id);
         } else {
-            $room = new WxRoom();
+            $room = new Product();
         }
         $room->name = $rawRoom->name;
         $room->price = $rawRoom->price;
