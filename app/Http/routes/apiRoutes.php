4596c7d7d559c9a437d68adb36d6d28ad5bc50ba
dreamingodd <memorywilllast@gmail.com>
2016-06-15 10:59:11 +0800

    Refactor routes.php

---
+++ app/Http/routes/apiRoutes.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * api route ，use for Vue，
+**/
+Route::group(['prefix' => 'api'], function () {
+    Route::get('home/recom/{id?}', 'Api\HomeController@getCasasByCityId');
+    Route::get('home/series/', 'Api\HomeController@getSeries');
+    Route::get('home/themes/', 'Api\HomeController@getThemes');
+    Route::get('casa/recom/{id?}', 'Api\CasaController@getCasasById');
+    Route::get('casa/slim/all', 'Api\CasaController@getSlimCasas');
+    Route::post('/recom/save/', 'Api\CasaController@save');
+    Route::post('/theme/change/', 'Api\ThemeController@setchange');
+    Route::post('/wechat/change/', 'Api\WechatController@setchange');
+    Route::get('theme/article/{id}', 'Api\ThemeController@getThemeArticle');
+    //民宿大全
+    Route::get('areas/{id?}','Api\AllCasaController@getAreasByCityId');
+    Route::get('casas/city/{id?}/areas/{areas?}','Api\AllCasaController@getCasas');
+    //民宿分享活动
+    Route::get('/eighteen/','Wx\Activity18Controller@sellist');
+});
---
+++ app/Http/routes/siteRoutes.php
@@ -0,0 +1,10 @@
+<?php
+Route::get('/', 'SiteController@index');
+Route::get('/area/{id}' , 'AreaController@show');
+Route::get('/casa/{id}' , 'CasaController@casaInfo');
+Route::get('/casaseries/{type}/{series?}', 'CasaSeriesController@casas');
+Route::get('/allcasa/{id?}','CasaController@allcasa');
+Route::get('/wechat/{type?}/{series?}', 'WechatController@index');
+Route::get('/oss/signature', 'OssController@execute');
+Route::get('/theme/{id}','ThemeController@show');
+Route::get('/about','SiteController@about');
