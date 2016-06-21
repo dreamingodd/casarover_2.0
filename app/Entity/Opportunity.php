abbd9ddaf643f45e25cc822bf7941ce18ef3f621
dreamingodd <memorywilllast@gmail.com>
2016-06-21 18:07:31 +0800

    recreate

---
+++ app/Entity/Opportunity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Opportunity extends Model
+{
+    public $timestamps = false;
+    protected $table = 'opportunity';
+    protected $fillable = ['left_quantity','order_item_id'];
+
+
+}
