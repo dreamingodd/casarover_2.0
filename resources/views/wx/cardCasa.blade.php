6d0657179e8a1b2e2b5ab2771bfd1eb9969d3a9d
van <draguo@sina.com>
2016-06-21 16:00:25 +0800

    申请他人度假卡step1

--- resources/views/wx/cardCasa.blade.php
+++ resources/views/wx/cardCasa.blade.php
@@ -9,28 +9,30 @@
     <img  src="/assets/images/logow.png" />
 @stop
 @section('body')
-        <div class="main">
-            <h2>度假卡民宿列表</h2>
-            {{--foreach--}}
-            @foreach($cardCasas as $casa)
-                <div class="case clear" >
-                    <div class="casecon clear">
-                        <img src="{{ $casa->photo_path }}" alt="">
-                        <div class="article">
-                            <h3>{{$casa->name}}</h3>
-                            <img src="/assets/images/sign.png" alt="" >
-                                <div class="articlecon">
-                                    <span>我的剩余间数：<i>{{$casa->Opportunity->left_quantity}}</i></span>
-                                    <div class="click">
-                                        <a href="/wx/user/cardBook/{{ $casa->id }}">预&nbsp;&nbsp;约</a>
-                                    </div>
-                            </div>
+    <div class="main">
+        <h2>度假卡民宿列表</h2>
+        {{--foreach--}}
+        @foreach($cardCasas as $casa)
+            <div class="case clear" >
+                <div class="casecon clear">
+                    <img src="{{ $casa->photo_path }}" alt="">
+                    <div class="article">
+                        <h3>{{$casa->name}}</h3>
+                        <div class="articlecon">
+                            <span>剩余间数：<i>{{$casa->Opportunity->left_quantity}}</i></span>
+                            @if($casa->Opportunity->left_quantity > 0)
+                                <a class="click" href="/wx/user/cardBook/{{ $casa->id }}">预&nbsp;&nbsp;约</a>
+                            @else
+                                {{--这个最好应该链接到民宿详情里面--}}
+                                <a class="click">不可预约</a>
+                            @endif
                         </div>
                     </div>
                 </div>
-                @endforeach
+            </div>
+        @endforeach
 
-        </div>
+    </div>
     <script>
     </script>
 @stop
--- resources/views/wx/cardEntry.blade.php
+++ resources/views/wx/cardEntry.blade.php
@@ -13,9 +13,23 @@
         <img src="/assets/images/card.png" alt="">
         <h2>度假卡</h2>
         <p>HOLIDAY CARD</p>
-        <input type="number" value="" class="form-control" placeholder="卡号">
+        <input type="number"  class="form-control" placeholder="卡号">
         {{--校验卡号正确与否--}}
-        <div class="confirm" onclick="document.location='/wx/user/cardCasa/1001884'">确定</div>
+        <div class="confirm" onclick="getlist()">确定</div>
     </div>
 
 @stop
+<script>
+    function getlist(){
+        var number = $(".main input").val();
+        console.log(number);
+        $.getJSON('/wx/api/user/cardCasa/'+number,function(data){
+            console.log(data);
+            if(data.code == 0){
+                window.location.href = '/wx/user/cardCasa/'+number;
+            }else{
+                alert("请检查卡号");
+            }
+        })
+    }
+</script>
