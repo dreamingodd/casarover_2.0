d65692701f995725d96b6db6e3b4ed20e0fc63dc
dreamingodd <memorywilllast@gmail.com>
2016-07-09 11:46:47 +0800

    crontab & pc wx login check path fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,11 +18,11 @@ require_once('routes/mobileRoutes.php');
 
 // Merchat
 Route::get('pc-wx-login/{redirectUrl?}', 'PcWxLoginController@login');
+Route::get('pc-wx-login/check/{code}', 'PcWxLoginController@check');
 Route::group(['prefix' => 'wx/pc-wx-login', 'middleware' => ['web', 'wx.auth']], function () {
     Route::get('option/{code}', 'PcWxLoginController@option');
     Route::get('approve/{code}', 'PcWxLoginController@approve');
     Route::get('reject/{code}', 'PcWxLoginController@reject');
-    Route::get('check/{code}', 'PcWxLoginController@check');
 });
 Route::group(['prefix' => 'merchant', 'middleware' => ['web', 'pc.wx']], function() {
     Route::get('/', 'Merchant\ReserveController@index');
