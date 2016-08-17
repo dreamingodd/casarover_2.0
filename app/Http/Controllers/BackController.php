f969e636dbc0139f91824d64fc6ac5b6a3eb9c3c
van <draguo@sina.com>
2016-08-17 16:56:26 +0800

    wrong

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -48,11 +48,8 @@ class backcontroller extends Controller
     public function deploy(Request $request)
     {
         if($request->token == env('DEPLOY_TOKEN')){
-
-            $result = Artisan::call('deploy',[
-                'msg'=>'ok'
-            ]);
-            return $result;
+            Artisan::call('deploy');
+            return 1;
         }else{
             return 0;
         }
